07935af9a1df9ee525bb953d8236283bf5cfe9e2
Chris West (Faux) <git@goeswhere.com>
2015-11-10 19:50:32 +0000

    start making the code awful

--- c.c
+++ c.c
@@ -3,36 +3,65 @@
 #include <stdint.h>
 #include <string.h>
 
-void arcfour_key_setup(uint8_t state[], const uint8_t key[], int len) {
-    for (int i = 0; i < 256; ++i) {
-        state[i] = i;
+static uint8_t key_len;
+static uint8_t *key;
+
+static uint8_t state[256];
+static uint8_t i = 0, j = 0;
+
+
+void arcfour_key_setup(void) {
+    uint8_t k = 0;
+    for (;;) {
+        state[k] = k;
+        ++k;
+        if (0 == k) { // overflow
+            break;
+        }
     }
 
-    uint8_t j = 0;
-    uint8_t t;
-    for (int i = 0; i < 256; ++i) {
-        j = (j + state[i] + key[i % len]) % 256;
-        t = state[i];
-        state[i] = state[j];
-        state[j] = t;
+    uint8_t kj = 0;
+    uint8_t ki = 0;
+    for (;;) {
+        kj = (kj + state[ki] + key[ki % key_len]) % 256;
+
+        // swap
+        uint8_t t = state[ki];
+        state[ki] = state[kj];
+        state[kj] = t;
+
+        ++ki;
+        if (ki == 0) { // overflow
+            break;
+        }
     }
 }
 
-void arcfour_generate_stream(uint8_t state[]) {
-    uint8_t i = 0, j = 0;
-    uint8_t t;
+uint8_t next(void) {
+    i = (i + 1) % 256;
+    j = (j + state[i]) % 256;
 
+    // swap
+    uint8_t t = state[i];
+    state[i] = state[j];
+    state[j] = t;
+
+    return state[(state[i] + state[j]) % 256];
+}
+
+void drop(void) {
+    for (int k = 0; k < 1024; ++k) {
+        next();
+    }
+}
+
+void arcfour_generate_stream(void) {
     for (;;) {
-        i = (i + 1) % 256;
-        j = (j + state[i]) % 256;
-        t = state[i];
-        state[i] = state[j];
-        state[j] = t;
         int got = getchar();
         if (EOF == got) {
             return;
         }
-        putchar(got ^ state[(state[i] + state[j]) % 256]);
+        putchar(got ^ next());
     }
 }
 
@@ -41,8 +70,12 @@ int main(int argc, char **argv) {
         fprintf(stderr, "usage: %s 'key'", argv[0]);
         return 1;
     }
-    uint8_t state[256];
-    arcfour_key_setup(state, argv[1], strlen(argv[1]));
-    arcfour_generate_stream(state);
+    key = argv[1];
+    key_len = strlen(key);
+
+    arcfour_key_setup();
+    
+    
+    arcfour_generate_stream();
 }
 
